c011380a6c0608401d4e39e1a6e4f153cabd21b0
abik50000 <abik50000@gmail.com>
2026-08-27 15:55:03 +0500

    fix

--- deploy/create-admin.sql
+++ deploy/create-admin.sql
@@ -3,3 +3,4 @@
 -- при наличии CLI лучше: php artisan make:filament-user)
 INSERT INTO `users` (`name`, `email`, `password`, `created_at`, `updated_at`)
 VALUES ('Admin', 'admin@parasat-aj.kz', '$2y$10$IfUjMP4N/cI.nfx4N6S/LucOF.NrN06QI3ltE7ARswKaPiMRc2sDC', NOW(), NOW());
+ 
